b0c6e059d1d9b7164ace96cc9bd07b26dfaeb6bb
Ahmed Maged <60134186+ahmedmaged3k1@users.noreply.github.com>
2024-01-31 01:01:20 +0200

    Repository Card Updates ( Added Stars Count )

---
+++ app/src/main/java/com/example/githubviewer/data/remote/dto/License.kt
@@ -0,0 +1,9 @@
+package com.example.githubviewer.data.remote.dto
+
+data class License(
+    val key: String,
+    val name: String,
+    val node_id: String,
+    val spdx_id: String,
+    val url: String
+)
--- app/src/main/java/com/example/githubviewer/presentation/common/ReposCard.kt
+++ app/src/main/java/com/example/githubviewer/presentation/common/ReposCard.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
@@ -23,18 +26,21 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.githubviewer.R
-import com.example.githubviewer.data.remote.dto.Owner
-import com.example.githubviewer.data.remote.dto.ReposResponse
+import com.example.githubviewer.data.remote.dto.License
+import com.example.githubviewer.data.remote.dto.OwnerX
+import com.example.githubviewer.data.remote.dto.RepoDetailsResponse
 import com.example.githubviewer.ui.theme.GithubViewerTheme
 import com.example.githubviewer.util.Dimens.extraSmallPadding
 import com.example.githubviewer.util.Dimens.reposCardSize
 
 
 @Composable
+
 fun repoCard(
     modifier: Modifier = Modifier,
-    repo: ReposResponse,
+    repo: RepoDetailsResponse,
     onclick: () -> Unit
 ) {
     val context = LocalContext.current
@@ -44,7 +50,6 @@ fun repoCard(
             .background(color = colorResource(id = R.color.input_background))
             .clickable { onclick() }
             .padding(vertical = extraSmallPadding)
-
     ) {
         Image(
             modifier = Modifier
@@ -58,6 +63,7 @@ fun repoCard(
             verticalArrangement = Arrangement.SpaceAround,
             modifier = Modifier
                 .height(reposCardSize)
+                .fillMaxWidth()
         ) {
             Text(
                 text = repo.name,
@@ -68,6 +74,30 @@ fun repoCard(
             )
 
             Spacer(modifier = Modifier.height(extraSmallPadding))
+
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(extraSmallPadding)
+            ) {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_star_24),
+                    contentDescription = "Star Icon",
+                    tint = colorResource(id = R.color.text_title)
+                )
+
+
+                Text(
+                    text = "${repo.stargazers_count}",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = colorResource(id = R.color.text_title),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+
+            // Description Text
             Text(
                 text = repo.description ?: "",
                 style = MaterialTheme.typography.bodyMedium,
@@ -77,21 +107,21 @@ fun repoCard(
             )
         }
     }
-
 }
+
 
 @Preview(showBackground = true)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 
 @Composable
 fun repoCardPreview() {
-    val fakeRepo = ReposResponse(
+    val fakeGitHubRepo = RepoDetailsResponse(
         id = 1,
         node_id = "MDEwOlJlcG9zaXRvcnkx",
         name = "grit",
         full_name = "mojombo/grit",
         private = false,
-        owner = Owner(
+        owner = OwnerX(
             login = "mojombo",
             id = 1,
             node_id = "MDQ6VXNlcjE=",
@@ -112,7 +142,7 @@ fun repoCardPreview() {
             site_admin = false
         ),
         html_url = "https://github.com/mojombo/grit",
-        description = "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object oriented read/write access to Git repositories via Ruby.",
+        description = "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object-oriented read/write access to Git repositories via Ruby.",
         fork = false,
         url = "https://api.github.com/repos/mojombo/grit",
         forks_url = "https://api.github.com/repos/mojombo/grit/forks",
@@ -150,10 +180,53 @@ fun repoCardPreview() {
         notifications_url = "https://api.github.com/repos/mojombo/grit/notifications{?since,all,participating}",
         labels_url = "https://api.github.com/repos/mojombo/grit/labels{/name}",
         releases_url = "https://api.github.com/repos/mojombo/grit/releases{/id}",
-        deployments_url = "https://api.github.com/repos/mojombo/grit/deployments"
+        deployments_url = "https://api.github.com/repos/mojombo/grit/deployments",
+        created_at = "2007-10-29T14:37:16Z",
+        updated_at = "2024-01-30T16:44:13Z",
+        pushed_at = "2023-08-17T19:15:41Z",
+        git_url = "git://github.com/mojombo/grit.git",
+        ssh_url = "git@github.com:mojombo/grit.git",
+        clone_url = "https://github.com/mojombo/grit.git",
+        svn_url = "https://github.com/mojombo/grit",
+        homepage = "http://grit.rubyforge.org/",
+        size = 7954,
+        stargazers_count = 1965,
+        watchers_count = 1965,
+        language = "Ruby",
+        has_issues = true,
+        has_projects = true,
+        has_downloads = true,
+        has_wiki = true,
+        has_pages = false,
+        has_discussions = false,
+        forks_count = 540,
+        mirror_url = null,
+        archived = false,
+        disabled = false,
+        open_issues_count = 35,
+        license = License(
+            key = "mit",
+            name = "MIT License",
+            spdx_id = "MIT",
+            url = "https://api.github.com/licenses/mit",
+            node_id = "MDc6TGljZW5zZTEz"
+        ),
+        allow_forking = true,
+        is_template = false,
+        web_commit_signoff_required = false,
+        topics = emptyList(),
+        visibility = "public",
+        forks = 540,
+        open_issues = 35,
+        watchers = 1965,
+        default_branch = "master",
+        temp_clone_token = null,
+        network_count = 540,
+        subscribers_count = 72
     )
+
     GithubViewerTheme {
-        repoCard(repo = fakeRepo) {
+        repoCard(repo = fakeGitHubRepo) {
 
         }
     }
